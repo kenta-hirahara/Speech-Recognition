dd5c9bf0717aebd3090784d36dab7ad5c5410b75
kenta-hirahara <kenta.hirahara@student.kit.edu>
2025-04-05 22:33:58 +0200

    load_env confined in instance/config.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from dotenv import load_dotenv
 
 def create_app():
-    load_dotenv()
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
